f494d1da1c18d43e29c9e2b81e12ba038ffde9ee
Mohamed shazan <90765408+Mohamedshazan@users.noreply.github.com>
2025-08-10 13:57:21 +0530

    Update asset_agreement.blade.php

--- resources/views/pdf/asset_agreement.blade.php
+++ resources/views/pdf/asset_agreement.blade.php
@@ -2,64 +2,128 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Asset Agreement</title>
+    <title>Asset Assignment Agreement</title>
     <style>
-        body { font-family: DejaVu Sans, sans-serif; font-size: 14px; }
-        h1, h2, h3 { margin: 0; padding: 0; }
-        .section { margin-bottom: 15px; }
+        body {
+            font-family: DejaVu Sans, sans-serif;
+            font-size: 14px;
+            line-height: 1.5;
+            margin: 20px;
+        }
+        .header-container {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 15px;
+        }
+        .logo {
+            height: 50px;
+        }
+        .company-info {
+            text-align: right;
+        }
+        .right-align-container {
+            text-align: right;
+        }
+        table {
+            width: auto;
+            border-collapse: collapse;
+            margin: 15px 0 15px auto;
+            text-align: left;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 5px 15px 5px 5px;
+        }
+        .underline {
+            text-decoration: underline;
+            display: inline-block;
+            min-width: 150px;
+        }
+        .divider {
+            border-top: 1px solid black;
+            margin: 15px 0;
+        }
+        .note {
+            font-size: 12px;
+            font-style: italic;
+        }
+        .make-model {
+            text-align: right;
+            margin: 15px 0;
+        }
     </style>
 </head>
 <body>
 
-<p>Brandix Apparel Solutions Limited,<br> Batticaloa.</p>
+<!-- Logo (left) and Company Header (right) -->
+<div class="header-container">
+    <img src="{{ public_path('images/logobr.jpg') }}" class="logo" alt="Brandix Logo">
+    <div class="company-info">
+        <strong>Brandix Apparel Solutions Limited,</strong><br>
+        <strong>Batticaloa.</strong>
+    </div>
+</div>
 
-<h3>Serial Numbers</h3>
-<table border="1" cellpadding="5" cellspacing="0" width="100%">
-    <tr>
-        <td><strong>Serial No</strong></td>
-        <td>{{ $asset->serial_number }}</td>
-    </tr>
-    <tr>
-        <td><strong>Battery</strong></td>
-        <td>IN BUILD</td>
-    </tr>
-    <tr>
-        <td><strong>Charger</strong></td>
-        <td>{{ $asset->charger_serial ?? 'N/A' }}</td>
-    </tr>
-</table>
+<!-- Right-aligned content container -->
+<div class="right-align-container">
+    <!-- Serial Numbers Table -->
+    <h3>Serial Numbers</h3>
+    <table>
+        <tr>
+            <td><strong>Serial No</strong></td>
+            <td>8CC9165F5V</td>
+        </tr>
+        <tr>
+            <td><strong>Battery</strong></td>
+            <td>IN BUILD</td>
+        </tr>
+        <tr>
+            <td><strong>Charger</strong></td>
+            <td>WFZJROF3UC33LY</td>
+        </tr>
+    </table>
 
-<p><strong>Make:</strong> {{ $asset->brand }}<br>
-<strong>Model:</strong> {{ $asset->model }}</p>
+    <!-- Make/Model -->
+    <div class="make-model">
+        <p><strong>Make:</strong> HP</p>
+        <p><strong>Model:</strong> HP ProDesk 400 G4 DM</p>
+    </div>
+</div>
 
-<p><strong>Date:</strong> {{ $date }}</p>
+<div class="divider"></div>
 
-<p><strong>Employee Name:</strong> {{ $user->name }}</p>
-
-<p>
-I am {{ $user->name }} acknowledge receipt of a {{ ucfirst($asset->asset_type) }} 
-and its accessories with a serial number of {{ $asset->serial_number }}. 
-I agree to return this {{ ucfirst($asset->asset_type) }} & all of its associated 
-equipment to Brandix Essentials Ltd at the time of resigning.
+<!-- Rest of the document (left-aligned) -->
+<p><strong>Date:</strong> 
+    <span class="underline">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+    2025-06/28
+    <span class="underline">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
 </p>
 
-<p>
-I am responsible for the full value of the {{ ucfirst($asset->asset_type) }} in the event 
-of loss (Equipment replaceable value at the time of lost) or its accessories.
-</p>
+<p><strong>Employee Name:</strong> <strong>Amila Silva</strong></p>
 
-<p>
-I agree to adhere to the Company's regulations and policies governing the use of the 
-stated in the Brandix Essentials ICT Policy.
-</p>
+<div class="divider"></div>
 
-<br><br>
-<p>____________________<br>Signature &nbsp;&nbsp;&nbsp; Date</p>
+<p><em>I am</em> <strong>Amila Silva</strong> 
+<em>acknowledge receipt of a Desktop and its accessories with a serial number of</em><br>
+8CC9165F5V. <em>I agree to return this Desktop and all of its associated equipment to Brandix Essentials Ltd at the time of resigning.</em></p>
 
-<small>
-*Note: Accessories include: Charger. Damages include: Breaking any part of the 
-{{ ucfirst($asset->asset_type) }} or its accessories, water damage, etc.
-</small>
+<p><em>I am responsible for the full value of the Desktop in the event of loss (Equipment replaceable value at the time of lost). Of its accessories. (*Note)</em></p>
+
+<p><em>I agree to adhere to the Company's regulations and policies governing the use of the stated in the Brandix Essentials ICT Policy.</em></p>
+
+<div class="divider"></div>
+
+<p><em>Signature</em><br>
+<em>Date</em></p>
+
+<div class="divider"></div>
+
+<p class="note"><strong>*Note.</strong><br>
+<em>Accessories include: Charger</em><br>
+<em>Damages include: Breaking any part of the Desktop or its accessories, water damage, etc.</em><br>
+<em>Computer Name - Desktop-T044IPH</em></p>
 
 </body>
 </html>
