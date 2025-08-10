82786a2d55f6fa0c0a5e5b649fdbcaf6d7b72fdd
Mohamed shazan <90765408+Mohamedshazan@users.noreply.github.com>
2025-08-10 18:37:49 +0530

    Update asset_agreement.blade.php

--- resources/views/pdf/asset_agreement.blade.php
+++ resources/views/pdf/asset_agreement.blade.php
@@ -6,7 +6,7 @@
     <style>
        @page {
     size: A4;
-    margin: 30mm; /* equal margin all sides */
+    margin: 1mm; /* equal margin all sides */
 }
 body {
     font-family: 'DejaVu Sans', sans-serif;
@@ -178,6 +178,7 @@ body {
     </div>
 </body>
 </html>
+
 
 
 
